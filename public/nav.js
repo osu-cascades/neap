577094b01447706e9e1254809f926c47ee593994
Michael Widler <MichaelWidler@Yahoo.com>
2020-11-01 16:59:08 -0800

    enable continue button on photo page

--- public/nav.js
+++ public/nav.js
@@ -1,6 +1,6 @@
 
-var goToSigButton = document.getElementByClassName("link-to-signature");
-goToSigButton.addEventListener("click", function(){open("signature.html", "_self")});
+var goToSigButton = document.getElementsByName("link-to-signature");
+goToSigButton[0].addEventListener("click", function(){open("signature.html", "_self")});
 
-var goToThankYouButton = document.getElementByClassName("link-to-thank-you");
-goToThankYouButton.addEventListener("click", function(){open("thankyou.html", "_self")});
+var goToThankYouButton = document.getElementsByName("link-to-thank-you");
+goToThankYouButton[0].addEventListener("click", function(){open("thankyou.html", "_self")});
