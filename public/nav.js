9b4cbf3d63ba24732f8c3fc45eaa63dd4e46546a
Michael Widler <MichaelWidler@Yahoo.com>
2020-11-01 16:50:37 -0800

    create nav.js

---
+++ public/nav.js
@@ -0,0 +1,6 @@
+
+var goToSigButton = document.getElementByClassName("link-to-signature");
+goToSigButton.addEventListener("click", function(){open("signature.html", "_self")});
+
+var goToThankYouButton = document.getElementByClassName("link-to-thank-you");
+goToThankYouButton.addEventListener("click", function(){open("thankyou.html", "_self")});
