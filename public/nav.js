2de34cedebe8f305f8ee4d61728cbf9a5b213127
Michael Widler <MichaelWidler@Yahoo.com>
2020-11-01 17:23:59 -0800

    create dummy page for signature with nav buttons

--- public/nav.js
+++ public/nav.js
@@ -1,6 +1,20 @@
 
+var goToFormButton = document.getElementsByName("link-to-form");
+if(goToFormButton.length > 0)
+	goToFormButton[0].addEventListener("click", function(){open("form_fields.html", "_self")});
+
 var goToSigButton = document.getElementsByName("link-to-signature");
-goToSigButton[0].addEventListener("click", function(){open("signature.html", "_self")});
+if(goToSigButton.length > 0)
+	goToSigButton[0].addEventListener("click", function(){open("signature.html", "_self")});
 
 var goToThankYouButton = document.getElementsByName("link-to-thank-you");
-goToThankYouButton[0].addEventListener("click", function(){open("thankyou.html", "_self")});
+if(goToThankYouButton.length > 0)
+	goToThankYouButton[0].addEventListener("click", function(){open("thankyou.html", "_self")});
+
+var goToHomeButton = document.getElementsByName("link-to-home");
+if(goToHomeButton.length > 0)
+	goToHomeButton[0].addEventListener("click", function(){open("index.html", "_self")});
+
+var goToPicButton = document.getElementsByName("link-to-pic-screen");
+if(goToPicButton.length > 0)
+	goToPicButton[0].addEventListener("click", function(){open("choose_pic_source.html", "_self")});
