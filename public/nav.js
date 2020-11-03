20b37ed5af115e1e70d7825f2464b71ab0ee9be3
Michael Widler <MichaelWidler@Yahoo.com>
2020-11-02 19:31:45 -0800

    create new_user.html

--- public/nav.js
+++ public/nav.js
@@ -3,6 +3,10 @@ var goToFormButton = document.getElementsByName("link-to-form");
 if(goToFormButton.length > 0)
 	goToFormButton[0].addEventListener("click", function(){open("form_fields.html", "_self")});
 
+var goToSigButton = document.getElementsByName("link-to-landing");
+if(goToSigButton.length > 0)
+	goToSigButton[0].addEventListener("click", function(){open("landing.html", "_self")});
+
 var goToSigButton = document.getElementsByName("link-to-signature");
 if(goToSigButton.length > 0)
 	goToSigButton[0].addEventListener("click", function(){open("signature.html", "_self")});
@@ -18,6 +22,10 @@ if(goToHomeButton.length > 0)
 var goToPicButton = document.getElementsByName("link-to-pic-screen");
 if(goToPicButton.length > 0)
 	goToPicButton[0].addEventListener("click", function(){open("choose_pic_source.html", "_self")});
+
+var goToPicButton = document.getElementsByName("link-to-new-user");
+if(goToPicButton.length > 0)
+	goToPicButton[0].addEventListener("click", function(){open("new_user.html", "_self")});
 
 var goToPicButton = document.getElementsByName("link-to-NI");
 if(goToPicButton.length > 0)
