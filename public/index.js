b687bb24238aead1b93b3b37d1e7976684dbcbe1
unknown <amberj.kolar@gmail.com>
2020-11-03 12:47:51 -0800

    Repairs for static html template pages

--- public/index.js
+++ public/index.js
@@ -1,8 +1,15 @@
 var linksToWelcome = document.getElementsByClassName("link-to-home");
+var linksToPic = document.getElementsByClassName("link-to-pic")
 var i;
 
 for (i = 0; i < linksToWelcome.length; i++) {
     linksToWelcome[i].addEventListener("click", function () {
-        open("index.html", "_self");
+        open("home.html", "_self");
+    });
+}
+
+for (i = 0; i < linksToPic.length; i++) {
+    linksToPic[i].addEventListener("click", function () {
+        open("choose_pic_source.html", "_self");
     });
 }
