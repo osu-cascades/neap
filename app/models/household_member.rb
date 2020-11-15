66530b3068e296c8dd9993b4cb1ab90d3a500b01
Michael Widler <MichaelWidler@Yahoo.com>
2020-11-14 20:06:55 -0800

    add data members for application record

---
+++ app/models/household_member.rb
@@ -0,0 +1,18 @@
+class HouseholdMember
+    def initialize()
+        @full_name
+        @dob
+        @ssn
+        @gender
+        @is_hispanic
+        @race_code
+        @is_OR_tribe
+        @education_code
+        @is_disabled
+        @is_veteran
+        @is_homebound
+        @is_SNAP
+        @is_OHP
+        @other_medical_insurance
+    end
+end
