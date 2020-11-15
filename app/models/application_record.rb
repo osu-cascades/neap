4d1e6628af7b445cd31c8a76e3459e739f4efc51
Michael Widler <MichaelWidler@Yahoo.com>
2020-11-14 19:45:29 -0800

    add class for household member

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def initialize()
+    @first_name = ""
+    @last_name = ""
+    @user_primary_key = -1
+  end
+
+  attr_accessor :user_primary_key
 end
