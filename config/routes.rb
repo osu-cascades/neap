c8c908ebec3ac57c9dc29349ffa1bc4be0c93496
Michael Widler <MichaelWidler@Yahoo.com>
2020-11-21 09:30:18 -0800

    remove branch change

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get "before_you_begin" => "static_pages#before_you_begin"
   get "application_main_info" => "static_pages#application_main_info"
   get "application_checklist" => "static_pages#application_checklist"
-  get "audit_page" => "static_pages#application_checklist"
 
   # Users
   # Using Devise RegistrationsController for public user creation/registration.
