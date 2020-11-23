ae178c8145c4468a679c2f907736b9f9880e90c7
Michael Widler <MichaelWidler@Yahoo.com>
2020-11-22 16:53:27 -0800

    add CRUD functions

--- app/models/energy_application.rb
+++ app/models/energy_application.rb
@@ -1,3 +1,24 @@
 class EnergyApplication < ApplicationRecord
-    
+    def getAllApplications
+        return EnergyApplication.all
+    end
+
+    def createApplication
+        enApp = EnergyApplication.create(clientID: current_user[:id])
+    end
+
+    def updateApplication(eaID, eaData)
+        enApp = EnergyApplication.find_by(id: eaID)
+        # update here
+        enApp.save
+    end
+
+    def destroyApplication(eaID)
+        enApp = EnergyApplication.find_by(id: eaID)
+        if enApp != nil
+            enApp.delete
+        end
+    end
+
+
 end
