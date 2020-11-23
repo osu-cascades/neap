e739f94bf7b96f55549b254cb57b6b3f5cb797b1
Michael Widler <MichaelWidler@Yahoo.com>
2020-11-22 16:27:25 -0800

    add energy application model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_090110) do
+ActiveRecord::Schema.define(version: 2020_11_23_001925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "energy_applications", force: :cascade do |t|
+    t.integer "clientID"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
