2379a68932367e99e423d81df5ada9f242c87b92
Michael Widler <MichaelWidler@Yahoo.com>
2021-01-17 20:02:37 -0800

    add columns for utilites table

--- db/migrate/20210118035934_create_utilities_table.rb
+++ db/migrate/20210118035934_create_utilities_table.rb
@@ -1,6 +1,10 @@
 class CreateUtilitiesTable < ActiveRecord::Migration[5.2]
   def change
     create_table :utilities_tables do |t|
+      t.integer :parent_application_id, null: false
+      t.string :utility_name
+      t.string :account_number
+      t.string :accountholder_name
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_035234) do
+ActiveRecord::Schema.define(version: 2021_01_18_035934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,13 @@ ActiveRecord::Schema.define(version: 2021_01_18_035234) do
     t.boolean "active", default: true, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "utilities_tables", force: :cascade do |t|
+    t.integer "parent_application_id", null: false
+    t.string "utility_name"
+    t.string "account_number"
+    t.string "accountholder_name"
   end
 
   add_foreign_key "energy_applications", "users"
