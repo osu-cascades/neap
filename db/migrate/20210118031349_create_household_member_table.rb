37b7362633b989a92964083f96d5e3448c4e8991
Michael Widler <MichaelWidler@Yahoo.com>
2021-01-17 19:25:50 -0800

    add household member migration to git tracking

---
+++ db/migrate/20210118031349_create_household_member_table.rb
@@ -0,0 +1,22 @@
+class CreateHouseholdMemberTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :household_members_table do |t|
+      t.integer :parent_application_id, null: false
+      t.string :name
+      t.date :dob
+      t.string :ssn
+      t.string :gender
+      t.boolean :hispanic
+      t.string :race
+      t.boolean :tribal_member
+      t.string :education
+      t.boolean :disabled
+      t.boolean :veteran
+      t.boolean :homebound
+      t.boolean :snap
+      t.boolean :ohp
+      t.string :other_insurance
+
+    end
+  end
+end
