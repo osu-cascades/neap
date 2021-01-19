e300a685f1de771ca9b122081c791a72b6ecd768
Michael Widler <MichaelWidler@Yahoo.com>
2021-01-18 19:20:45 -0800

    add migrate file to git tracking

---
+++ db/migrate/20210118040459_create_declaration_of_household_income_table.rb
@@ -0,0 +1,11 @@
+class CreateDeclarationOfHouseholdIncomeTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :declaration_of_household_income_tables do |t|
+      t.integer :parent_application_id, null: false
+      t.string :member_name
+      t.money :informal_income_amount
+      t.string :informal_income_source
+      t.boolean :is_highschooler
+    end
+  end
+end
