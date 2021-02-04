6f355534aa036e17f3d7d59f37cdfbc491d909dd
Michael Widler <MichaelWidler@Yahoo.com>
2021-02-03 16:58:00 -0800

    set max household members to 8

--- app/models/energy_application.rb
+++ app/models/energy_application.rb
@@ -1,10 +1,9 @@
 class EnergyApplication < ApplicationRecord
 
   belongs_to :user
-  MAX_HOUSEHOLD_MEMBERS = 8.freeze
 
-    def to_s
-      "Application #{id}"
-    end
+  def to_s
+    "Application #{id}"
+  end
 
 end
