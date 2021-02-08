bc158c7200d4eb7014985a540078c58c515f00a6
Michael Widler <MichaelWidler@Yahoo.com>
2021-02-08 13:27:19 -0800

    expand household member table

--- db/migrate/20210118035234_household_member_income.rb
+++ db/migrate/20210118035234_household_member_income.rb
@@ -3,6 +3,19 @@ class HouseholdMemberIncome < ActiveRecord::Migration[5.2]
     create_table :household_member_incomes do |t|
       t.integer :parent_application_id, null: false
       t.string :name
+      t.date :dob
+      t.string :ssn
+      t.string :gender
+      t.boolean :hispanic
+      t.string :race
+      t.boolean :or_tribe
+      t.string :education
+      t.boolean :disabled
+      t.boolean :veteran
+      t.boolean :homebound
+      t.boolean :snap
+      t.boolean :ohp
+      t.string :other_insurance
       t.string :income_type
     end
   end
