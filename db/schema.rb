fabea59c31d3b9e7330f9553dcb687a0a0cf5570
Michael Widler <MichaelWidler@Yahoo.com>
2021-02-09 14:13:50 -0800

    modify schema

--- db/schema.rb
+++ db/schema.rb
@@ -65,29 +65,6 @@ ActiveRecord::Schema.define(version: 2021_02_05_095822) do
     t.index ["user_id"], name: "index_energy_applications_on_user_id"
   end
 
-  create_table "household_member_incomes", force: :cascade do |t|
-    t.integer "parent_application_id", null: false
-    t.string "name"
-    t.date "dob"
-    t.string "ssn"
-    t.string "gender"
-    t.boolean "hispanic"
-    t.string "race"
-    t.boolean "or_tribe"
-    t.string "education"
-    t.boolean "disabled"
-    t.boolean "veteran"
-    t.boolean "homebound"
-    t.boolean "snap"
-    t.boolean "ohp"
-    t.string "other_insurance"
-    t.string "income_type"
-    t.boolean "over_18_no_formal_income"
-    t.decimal "informal_income_amount"
-    t.string "informal_income_source"
-    t.boolean "in_high_school"
-  end
-
   create_table "household_members", force: :cascade do |t|
     t.integer "parent_application_id", null: false
     t.string "name"
