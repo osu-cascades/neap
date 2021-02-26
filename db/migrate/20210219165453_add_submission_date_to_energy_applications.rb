a588d728fc4420881acbd7d7c3410562fdc99dad
Michael Widler <MichaelWidler@Yahoo.com>
2021-02-25 18:30:24 -0800

    correct location of updating timestamp to datetime

--- db/migrate/20210219165453_add_submission_date_to_energy_applications.rb
+++ db/migrate/20210219165453_add_submission_date_to_energy_applications.rb
@@ -1,5 +1,5 @@
 class AddSubmissionDateToEnergyApplications < ActiveRecord::Migration[5.2]
   def change
-    add_column :energy_applications, :submission_date, :date
+    add_column :energy_applications, :submission_date, :datetime
   end
 end
