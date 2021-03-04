7faea410c3e2ae99a3f85f10b0925746b02fe03b
Michael Widler <MichaelWidler@Yahoo.com>
2021-03-04 13:43:29 -0800

    add admin flag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_19_165453) do
+ActiveRecord::Schema.define(version: 2021_03_04_213437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 2021_02_19_165453) do
     t.string "last_name", null: false
     t.integer "role", default: 0, null: false
     t.boolean "active", default: true, null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
