df299b51c8f91918af8e63ef049dd6b023d12f0a
Michael Widler <MichaelWidler@Yahoo.com>
2021-03-05 09:50:46 -0800

    correct contact_info_string field to contact_info, set parent_application to nono-null

--- db/migrate/20210305174621_create_contact_methods.rb
+++ db/migrate/20210305174621_create_contact_methods.rb
@@ -1,7 +1,7 @@
 class CreateContactMethods < ActiveRecord::Migration[5.2]
   def change
     create_table :contact_methods do |t|
-      t.int :parent_application
+      t.int :parent_application, null: false
       t.string :first_name
       t.string :last_name
       t.string :contact_method
