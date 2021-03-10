99524fe9be4ef5db692ba64521eedd75dddeb4f6
Michael Widler <MichaelWidler@Yahoo.com>
2021-03-10 13:42:29 -0800

    consolidate contact table into energy_application

--- db/migrate/20210219165453_add_submission_date_to_energy_applications.rb
+++ db/migrate/20210219165453_add_submission_date_to_energy_applications.rb
@@ -2,5 +2,7 @@ class AddSubmissionDateToEnergyApplications < ActiveRecord::Migration[5.2]
   def change
     add_column :energy_applications, :submission_date, :datetime
     add_column :energy_applications, :confirmation_date, :datetime
+    add_column :energy_applications, :contact_type, :text
+    add_column :energy_applications, :contact_info, :text
   end
 end
