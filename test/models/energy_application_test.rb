65c7295f71a558d40d7596688ac9ee21eb450763
Yong Bakos <ybakos@humanoriented.com>
2021-03-28 20:58:27 -0700

    EnergyApplication: Test attributes.

--- test/models/energy_application_test.rb
+++ test/models/energy_application_test.rb
@@ -2,10 +2,6 @@ require 'test_helper'
 
 class EnergyApplicationTest < ActiveSupport::TestCase
 
-  test 'belongs to a user' do
-    assert_respond_to(EnergyApplication.new, :user)
-  end
-
   test 'attributes' do
     application = EnergyApplication.new
     assert_respond_to(application, :phone_number)
@@ -14,6 +10,47 @@ class EnergyApplicationTest < ActiveSupport::TestCase
     assert_respond_to(application, :city)
     assert_respond_to(application, :county)
     assert_respond_to(application, :zip)
+    assert_respond_to(application, :first_name)
+    assert_respond_to(application, :last_name)
+    assert_respond_to(application, :mailing_address_street)
+    assert_respond_to(application, :mailing_address_city)
+    assert_respond_to(application, :mailing_address_state)
+    assert_respond_to(application, :mailing_address_zip)
+    assert_respond_to(application, :mailing_address_county)
+    assert_respond_to(application, :type_of_dwelling)
+    assert_respond_to(application, :residence_status)
+    assert_respond_to(application, :types_of_heat)
+    assert_respond_to(application, :primary_heat_source)
+    assert_respond_to(application, :secondary_heat_source)
+    assert_respond_to(application, :utility_company)
+    assert_respond_to(application, :household_member_types_of_income)
+    assert_respond_to(application, :utility_names)
+    assert_respond_to(application, :utility_account_numbers)
+    assert_respond_to(application, :utility_account_names)
+    assert_respond_to(application, :household_members_over_18_without_income)
+    assert_respond_to(application, :how_necessities_are_paid)
+    assert_respond_to(application, :household_impacted_by_covid)
+    assert_respond_to(application, :covid_loss_of_employment)
+    assert_respond_to(application, :covid_reduction_of_hours)
+    assert_respond_to(application, :covid_vulnerable_population)
+    assert_respond_to(application, :covid_loss_of_childcare)
+    assert_respond_to(application, :covid_other_situation)
+    assert_respond_to(application, :covid_expenses)
+    assert_respond_to(application, :phone_type)
+    assert_respond_to(application, :household_type)
+    assert_respond_to(application, :email_address)
+    assert_respond_to(application, :submission_date)
+    assert_respond_to(application, :confirmation_date)
+    assert_respond_to(application, :contact_type)
+    assert_respond_to(application, :contact_info)
+  end
+
+  test 'belongs to a user' do
+    assert_respond_to(EnergyApplication.new, :user)
+  end
+
+  test '#to_s' do
+    assert_match(EnergyApplication.new.to_s, "Application ")
   end
 
 end
