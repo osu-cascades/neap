966df47b2a6b9054bce2c9858e80e4527a8db46b
Yong Bakos <ybakos@humanoriented.com>
2021-04-16 11:01:58 -0700

    Static Pages: Remove unused pages, routes, actions.
    References #89.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,4 @@
 class StaticPagesController < ApplicationController
   skip_before_action :authenticate_user!
 
-  def application_main_info; end
-  def home; end
-  def before_you_begin; end
-  def application_checklist; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   get "/before_you_begin" => "static_pages#before_you_begin"
-  get "/application_main_info" => "static_pages#application_main_info"
   get "/application_checklist" => "static_pages#application_checklist"
   get "/screening_questions" => "static_pages#screening_questions"
-  get "/household_questions" => "static_pages#application_household_info"
-  get "/covid_questions" => "static_pages#application_covid_questions"
   get "/exit_page" => "static_pages#exit_page"
 
   resources :energy_applications
